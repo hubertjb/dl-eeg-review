8906ff92ef8b5419c1e76df2152e940baac46335
Yannick Roy <ymyannick@gmail.com>
2018-10-28 00:02:43 -0400

    Tex Table Automation now supports ";" separated fields.  (e.g. 1 paper can use more than 1 dataset)

--- tex_table.py
+++ tex_table.py
@@ -4,22 +4,42 @@
 import pandas as pd
 import tex_utils
 
-papers = pd.read_csv('./paperx.csv')
+#papers = pd.read_csv('./paperx.csv')
+papers = pd.read_csv('./papers.csv', header=1)
 
-domains = papers['Domain'].unique()
+papers = papers.dropna(axis=1, how='all')
+
+domains = set(papers['Domain'].dropna(axis=0, how='all'))
 
 nested_datasets = {}
 
 for domain in domains:
-    sub_papers = papers[papers['Domain'] == domain]
-    sub_datasets = sub_papers['Name'].unique()
-    nested_list = {k:list(sub_papers[sub_papers['Name'] == k]['Citation']) for k in sub_datasets}
+    print('Domain:' + domain)
 
+    sub_papers = papers[papers['Domain'] == domain]
+    sub_datasets = sub_papers['Dataset name'].dropna(axis=0, how='all')
+
+    # 1 Paper might used multiple datasets
+    l = [dsname.split(";\n") for dsname in sub_datasets]
+    sub_datasets = set([item for sublist in l for item in sublist])
+
+    # Handle exception to make it "prettier".
+    # Exception 1: BCI Competition Datasets.
+    # Exception 2: Datasets used only once.
+
+    nested_list = {k: list(sub_papers[sub_papers['Dataset name'].str.contains(k)]['Citation']) for k in sub_datasets}
     nested_datasets[domain] = nested_list
 
-
+print('LaTeX!')
 template = tex_utils.get_template('./table_template.tex')
 variable_dict = {'datasets': nested_datasets}
 texstr = tex_utils.compile_pdf_from_template(template, variable_dict, './dataset_table.pdf')
+print('Done!')
 
 print(texstr)
+
+
+flat_list = set([])
+for sublist in l:
+    for item in sublist:
+        print(item)
